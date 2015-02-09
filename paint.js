aa3368fcf2e619e2a7bff2227b7eeb958b40841f
Yuchen Wang <yulon@me.com>
2015-02-10 03:37:40 +0800

    For webkit

--- paint.js
+++ paint.js
@@ -1,16 +1,36 @@
-HTMLElement.prototype.paint = function(canvasContextType, painter){
-	if (this._cav == null) {
-		this._cav = document.createElement("canvas");
-		this._cav_ctxs = {};
+if (document.getCSSCanvasContext) {
+	HTMLElement.prototype.paint = function(canvasContextType, painter){
+		if (this._paint_ctx_type != canvasContextType) {
+			this._paint_ctx_type = canvasContextType;
+			this._paint_ctx_id = "_" + Math.floor(Date.parse(new Date()) * Math.random()).toString(16);
+			this._paint_ctx = document.getCSSCanvasContext("2d", this._paint_ctx_id, this.offsetWidth, this.offsetHeight);	
+		} else {
+			if (this._paint_ctx.canvas.width != this.offsetWidth) {
+				this._paint_ctx.canvas.width = this.offsetWidth;
+			};
+			if (this._paint_ctx.canvas.height != this.offsetHeight) {
+				this._paint_ctx.canvas.height = this.offsetHeight;
+			};
+		};
+		painter(this._paint_ctx);
+		this.style.backgroundImage = "-webkit-canvas(" + this._paint_ctx_id + ")";
 	};
-	if (this._cav.width != this.offsetWidth) this._cav.width = this.offsetWidth;
-	if (this._cav.height != this.offsetHeight) this._cav.height = this.offsetHeight;
-
-	if (this._cav_ctxs[canvasContextType] == null) {
-		this._cav_ctxs[canvasContextType] = this._cav.getContext(canvasContextType);
+} else {
+	HTMLElement.prototype.paint = function(canvasContextType, painter){
+		if (this._paint_ctx_type != canvasContextType) {
+			this._paint_ctx_type = canvasContextType;
+			this._paint_ctx = document.createElement("canvas").getContext(canvasContextType);
+			this._paint_ctx.canvas.width = this.offsetWidth;
+			this._paint_ctx.canvas.height = this.offsetHeight;
+		} else {
+			if (this._paint_ctx.canvas.width != this.offsetWidth) {
+				this._paint_ctx.canvas.width = this.offsetWidth;
+			};
+			if (this._paint_ctx.canvas.height != this.offsetHeight) {
+				this._paint_ctx.canvas.height = this.offsetHeight;
+			};
+		};
+		painter(this._paint_ctx);
+		this.style.backgroundImage = "url(\"" + this._paint_ctx.canvas.toDataURL("image/png") + "\")";
 	};
-
-	painter(this._cav_ctxs[canvasContextType]);
-
-	this.style.backgroundImage = "url(\"" + this._cav.toDataURL("image/png") + "\")";
 };
