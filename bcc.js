ce68331f2ff6b547a6358896d3b2010a066eb321
Yuchen Wang <yulon@me.com>
2015-02-10 22:53:28 +0800

    Change offsetSize to clientSize

--- bcc.js
+++ bcc.js
@@ -3,14 +3,14 @@ HTMLElement.prototype.getBackgroundCanvasContext = function(ctxType){
 
 	if (document.getCSSCanvasContext) {
 		var ctxId = "_" + Math.floor(Date.parse(new Date()) * Math.random()).toString(16);
-		ctx = document.getCSSCanvasContext(ctxType, ctxId, this.offsetWidth, this.offsetHeight);
+		ctx = document.getCSSCanvasContext(ctxType, ctxId, this.clientWidth, this.clientHeight);
 		getImg = function() {
 			return "-webkit-canvas(" + ctxId + ")";
 		};
 	} else {
 		ctx = document.createElement("canvas").getContext(this._paint_ctx_type);
-		ctx.canvas.width = this.offsetWidth;
-		ctx.canvas.height = this.offsetHeight;
+		ctx.canvas.width = this.clientWidth;
+		ctx.canvas.height = this.clientHeight;
 		getImg = function() {
 			return "url(\"" + ctx.canvas.toDataURL("image/png") + "\")";
 		};
@@ -19,11 +19,11 @@ HTMLElement.prototype.getBackgroundCanvasContext = function(ctxType){
 	var ele = this;
 
 	ctx.update = function() {
-		if (this.canvas.width != ele.offsetWidth) {
-			this.canvas.width = ele.offsetWidth;
+		if (this.canvas.width != ele.clientWidth) {
+			this.canvas.width = ele.clientWidth;
 		};
-		if (this.canvas.height != ele.offsetHeight) {
-			this.canvas.height = ele.offsetHeight;
+		if (this.canvas.height != ele.clientHeight) {
+			this.canvas.height = ele.clientHeight;
 		};
 		ele.style.backgroundImage = getImg();
 	};
